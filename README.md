099fef9b582730bd3c1335f8411f14f9c81dbc74
amandapickering <pickering.amanda@gmail.com>
2015-02-09 00:13:15 -0500

    more cleanup and readme update

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ a simple echo server that doesn't take your crap. made to learn about sockets an
 
 ## usage:
 
+start the server:
+```py
 python echo_server.py
+```
+
+connect via netcat:
+```nc [your ip address] [port number provided by server]```
+
+chat away! (and don't be a jerk.)
--- echo_server.py
+++ echo_server.py
@@ -1,47 +1,49 @@
-import socket, random, re
+import socket
+import random
+import re
 
 PORT = random.randint(6000, 9000)
 MSGLEN = 1024
 MAX_LISTEN = 5
 
 MEAN_STUFF = ['suck',
-				'boo',
-				'hate',
-				'lame',
-				'worst',
-				'go away',
-				'stink',
-				'shut up',
-				'mean']
+              'boo',
+              'hate',
+              'lame',
+              'worst',
+              'go away',
+              'stink',
+              'shut up',
+              'mean']
 
 mean_regex = re.compile(r'\b(?:%s)\b' % '|'.join(MEAN_STUFF), re.IGNORECASE)
 
 if __name__ == '__main__':
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.bind(('', PORT))
-	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server.listen(MAX_LISTEN)
-	
-	print 'connecting to port %s' % PORT
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('', PORT))
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.listen(MAX_LISTEN)
 
-	(client, address) = server.accept()
+    print 'connecting to port %s' % PORT
 
-	while True:
-		data = client.recv(MSGLEN)
-		if data.strip().lower() == 'logout':
-			client.send('logged out\n')
-			print 'logged out\n'
-			client.close()
-			break
-		elif mean_regex.search(data.strip().lower()):
-			client.send('fine :(\n')
-			print 'logged out'
-			client.close()
-			break
-		elif data.strip().lower() == 'hi':
-			client.send('why hello there!\n')
-		elif data.strip().lower() == 'there is some updog on the server':
-			client.send('what\'s updog?\n') # sorry
-		elif data:
-			print 'received %s' % data
-			client.send(data)
+    (client, address) = server.accept()
+
+    while True:
+        data = client.recv(MSGLEN)
+        if data.strip().lower() == 'logout':
+            client.send('logged out\n')
+            print 'logged out\n'
+            client.close()
+            break
+        elif mean_regex.search(data.strip().lower()):
+            client.send('fine :(\n')
+            print 'logged out'
+            client.close()
+            break
+        elif data.strip().lower() == 'hi':
+            client.send('why hello there!\n')
+        elif data.strip().lower() == 'there is some updog on the server':
+            client.send('what\'s updog?\n')  # sorry
+        elif data:
+            print 'received %s' % data
+            client.send(data)
